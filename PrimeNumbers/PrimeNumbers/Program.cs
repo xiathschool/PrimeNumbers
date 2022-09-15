659a4cfc1d51a6427d21b229600dbb06c12ff602
Andrew Xia <xiathschool@gmail.com>
2022-09-15 12:03:52 -0400

    Implemented Sieve of Eratosthenes

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO.IsolatedStorage;
+using System.Diagnostics;
+
+int numbers = 2000000;
+bool[] isNotPrime = new bool[2 * (int) (numbers * Math.Log(numbers))];
+
+Stopwatch s = Stopwatch.StartNew();
+
+for (int i = 2; i < isNotPrime.Length; i++)
+{
+    if (!isNotPrime[i])
+    {
+        numbers--;
+        if (numbers == 0)
+        {
+            Console.WriteLine(i);
+            break;
+        }
+        for (int j = i * 2; j < isNotPrime.Length; j += i)
+        {
+            isNotPrime[j] = true;
+        }
+    }
+}
+s.Stop();
+Console.WriteLine(s.ElapsedMilliseconds);
